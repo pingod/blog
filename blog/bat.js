0fcf2f448045c6ef8c3fea550f1ccfc0bfa53e39
阎王 <yanwang.lj@alibaba-inc.com>
2018-02-28 19:55:01 +0800

    replace cnblog img to local

--- blog/bat.js
+++ blog/bat.js
@@ -23,26 +23,30 @@ console.log(err);
 function deal(file) {
   var content = fs.readFileSync(file).toString();
   var date = /(\d{4})-(\d{2})-(\d{2})/.exec(file);
-  var content2 = content.replace(/<img src="([^"]+?)"/g, function ($0, $1) {
-    if ($1.indexOf('//') === -1 || $1 === "//img.alicdn.com/tfs/TB1oyqGa_tYBeNjy1XdXXXXyVXa-300-300.png") return;
-    var url = /^http/.test($1) ? $1 : `http:${$1}`;
+  var content2 = content.replace(/\!\[([^\]]+?])\(([\s\S]+?)\)/g, function ($0, $1, $2) {
+    if ($2.indexOf('//') === -1 || $2 === "//img.alicdn.com/tfs/TB1oyqGa_tYBeNjy1XdXXXXyVXa-300-300.png") return $0;
+    if ($2.indexOf('www.barretlee.com') > -1) {
+      var p = '/blogimgs/' + $2.split('/blogimgs/')[1];
+      return `![${$1}](${p})`;
+    }
+    var url = /^http/.test($2) ? $2 : `http:${$2}`;
     var name = url.split('/');
     name = name[name.length - 1];
     if (url.indexOf('sinaimg') > -1 && url.indexOf('.') === -1) {
       name += '.jpg';
     }
-    // console.log(`<img src="//img.alicdn.com/tfs/TB1oyqGa_tYBeNjy1XdXXXXyVXa-300-300.png" data-original="/blogimgs/${date[1]}/${date[2]}/${date[3]}/${name}" data-source="${$1}"`);
+    // console.log(`![${$1}](/blogimgs/${date[1]}/${date[2]}/${date[3]}/${name})<!--<source src="${$2}">-->`);
     mkDirByPathSync(`./src/blogimgs/${date[1]}/${date[2]}/${date[3]}`);
     try {
       exexSync(`wget -O /Users/barretlee/work/blogsys/blog/src/blogimgs/${date[1]}/${date[2]}/${date[3]}/${name} ${url}`);
     } catch(e) {
-      err.push(file + ' ' + $1);
+      err.push(file + ' ' + $2);
     }
     // // 删除错误目录下的文件
     // if (fs.existsSync(`./src/blogimgs/${name}`)) {
     //   fs.unlink(`./src/blogimgs/${name}`);
     // }
-    return `<img src="//img.alicdn.com/tfs/TB1oyqGa_tYBeNjy1XdXXXXyVXa-300-300.png" data-original="/blogimgs/${date[1]}/${date[2]}/${date[3]}/${name}" data-source="${$1}"`;
+    return `![${$1}](/blogimgs/${date[1]}/${date[2]}/${date[3]}/${name})<!--<source src="${$2}">-->`;
   });
   // if (content !== content2) {
   //   fs.writeFileSync(file, content2);
