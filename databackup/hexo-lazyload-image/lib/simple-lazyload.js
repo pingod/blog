f4d62500657ca0377dfb818a64517399c95fd2a7
阎王 <yanwang.lj@alibaba-inc.com>
2018-03-01 11:09:32 +0800

    backup

--- databackup/hexo-lazyload-image/lib/simple-lazyload.js
+++ databackup/hexo-lazyload-image/lib/simple-lazyload.js
@@ -3,36 +3,39 @@
 
     function elementInViewport(el) {
         var rect = el.getBoundingClientRect();
+        var W = window.innerWidth || document.documentElement.clientWidth;
+        var H = window.innerHeight || document.documentElement.clientHeight;
         return (
-            rect.top >= 0
-            && rect.left >= 0
-            && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+            (rect.top >= 0 || rect.left >= 0
+                || rect.top < 0 && (rect.top + rect.height) >= 0
+                || rect.left < 0 && (rect.left + rect.width >= 0))
+                && rect.top <= H && rect.left <= W
         );
     }
     function loadImage(el, fn) {
-        var img = new Image()
-            , src = el.getAttribute('data-original')
-            , isBg = el.getAttribute('data-isbg');
+        var src = el.getAttribute('data-original');
+        var inited = el.getAttribute('data-init');
+        if (inited === 'yes') return;
+        var isBg = el.getAttribute('data-isbg');
         src = src.replace(/^https?:/, '');
-        // img.onload = function () {
-            if (isBg) {
-                el.style.backgroundImage = src;
-            }
-            setTimeout(function () {
-                el.setAttribute('src', src);
-                fn ? fn() : null;
-            }, 10);
-        // };
-        // img.src = src;
+        if (isBg) {
+            el.style.backgroundImage = src;
+        } else {
+            el.setAttribute('src', src);
+        }
+        el.setAttribute('data-init', 'yes');
+        fn ? fn() : null;
     }
 
     function processImages() {
         for (var i = 0; i < images.length; i++) {
-            if (elementInViewport(images[i])) {
-                loadImage(images[i], function () {
-                    images.splice(i, i);
-                });
-            }
+            !function(i) {
+                if (elementInViewport(images[i])) {
+                    loadImage(images[i], function () {
+                        images.splice(i, 1);
+                    });
+                }
+            }(i);
         };
     }
 
